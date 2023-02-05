90e10b6d85f605f2247d536cc4770c85350f1fa3
starlord678 <thomasnolasque@cmail.carleton.ca>
2023-02-05 05:42:35 -0500

    UI significant (relative) improvement

--- Textbox.gd
+++ Textbox.gd
@@ -1,10 +1,7 @@
 extends CanvasLayer
 
-const CHAR_READ_RATE = 0.05
+const CHAR_READ_RATE = 0.025
 
-onready var textbox_container = $TextboxContainer
-onready var start_symbol = $TextboxContainer/MarginContainer/HBoxContainer/Start
-onready var end_symbol = $TextboxContainer/MarginContainer/HBoxContainer/End
 onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label
 
 enum State {
@@ -18,7 +15,6 @@ var text_queue = []
 
 func _ready():
 	print("Starting state: State.READY")
-	hide_textbox()
 	queue_text("Excuse me wanderer where can I find the bathroom?")
 	queue_text("Why do we not look like the others?")
 	queue_text("Because we are free assets from opengameart!")
@@ -33,32 +29,20 @@ func _process(delta):
 			if Input.is_action_just_pressed("ui_accept"):
 				label.percent_visible = 1.0
 				$Tween.remove_all()
-				end_symbol.text = "v"
 				change_state(State.FINISHED)
 		State.FINISHED:
 			if Input.is_action_just_pressed("ui_accept"):
-				change_state(State.READY)
-				hide_textbox()
+				# change_state(State.READY)
+				pass
 
 func queue_text(next_text):
 	text_queue.push_back(next_text)
-
-func hide_textbox():
-	start_symbol.text = ""
-	end_symbol.text = ""
-	label.text = ""
-	textbox_container.hide()
-
-func show_textbox():
-	start_symbol.text = "*"
-	textbox_container.show()
 
 func display_text():
 	var next_text = text_queue.pop_front()
 	label.text = next_text
 	label.percent_visible = 0.0
 	change_state(State.READING)
-	show_textbox()
 	$Tween.interpolate_property(label, "percent_visible", 0.0, 1.0, len(next_text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	$Tween.start()
 
@@ -73,5 +57,4 @@ func change_state(next_state):
 			print("Changing state to: State.FINISHED")
 
 func _on_Tween_tween_completed(object, key):
-	end_symbol.text = "v"
 	change_state(State.FINISHED)
