b8a5a66f2938a5547a511d6596631db72be90bad
starlord678 <thomasnolasque@cmail.carleton.ca>
2023-02-05 14:44:07 -0500

    Even More UI

--- Background.gd
+++ Background.gd
@@ -1,71 +1,25 @@
 extends Node2D
 
-const CHAR_READ_RATE = 0.05
-
-onready var textbox_container = $TextboxContainer
-onready var label = $TextboxContainer/MarginContainer/HBoxContainer/Label
-
-enum State {
-	READY,
-	READING,
-	FINISHED
-}
-
-var current_state = State.READY
-var text_queue = []
-
 func _ready():
-	print("Starting state: State.READY")
-	hide_textbox()
-	queue_text("Excuse me wanderer where can I find the bathroom?")
-	queue_text("Why do we not look like the others?")
-	queue_text("Because we are free assets from opengameart!")
-	queue_text("Thanks for watching!")
+	$Rows/Textbox._ready()
 
 func _process(delta):
-	match current_state:
-		State.READY:
-			if !text_queue.empty():
-				display_text()
-		State.READING:
-			if Input.is_action_just_pressed("ui_accept"):
-				label.percent_visible = 1.0
-				$Tween.remove_all()
-				change_state(State.FINISHED)
-		State.FINISHED:
-			if Input.is_action_just_pressed("ui_accept"):
-				change_state(State.READY)
-				hide_textbox()
+	$Rows/Textbox._process(delta)
 
 func queue_text(next_text):
-	text_queue.push_back(next_text)
+	$Rows/Textbox.queue_text(next_text)
 
 func hide_textbox():
-	label.text = ""
-	textbox_container.hide()
+	$Rows/Textbox.hide_textbox()
 
 func show_textbox():
-
-	textbox_container.show()
+	$Rows/Textbox.show_textbox()
 
 func display_text():
-	var next_text = text_queue.pop_front()
-	label.text = next_text
-	label.percent_visible = 0.0
-	change_state(State.READING)
-	show_textbox()
-	$Tween.interpolate_property(label, "percent_visible", 0.0, 1.0, len(next_text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	$Tween.start()
+	$Rows/Textbox.display_text()
 
 func change_state(next_state):
-	current_state = next_state
-	match current_state:
-		State.READY:
-			print("Changing state to: State.READY")
-		State.READING:
-			print("Changing state to: State.READING")
-		State.FINISHED:
-			print("Changing state to: State.FINISHED")
+	$Rows/Textbox.change_state(next_state)
 
 func _on_Tween_tween_completed(object, key):
-	change_state(State.FINISHED)
+	$Rows/Textbox._on_Tween_tween_completed(object, key)
